3672bc6b29323b69e56dfff2289954382ad23f11
sachetwasti1997 <sachetwasti61@gmail.com>
2021-05-25 14:05:25 +0530

    first commit

---
+++ src/test/java/com/sachet/recipies/RecipiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachet.recipies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
